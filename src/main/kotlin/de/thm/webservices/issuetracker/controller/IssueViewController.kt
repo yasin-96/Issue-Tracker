1dcf04ab08a63534421c1a93292c6aff2d553338
Serkan <serkan.demir@mni.thm.de>
2020-08-18 16:17:47 +0200

    IssueViewController.kt is working now

--- src/main/kotlin/de/thm/webservices/issuetracker/controller/IssueViewController.kt
+++ src/main/kotlin/de/thm/webservices/issuetracker/controller/IssueViewController.kt
@@ -2,11 +2,13 @@ package de.thm.webservices.issuetracker.controller
 
 import de.thm.webservices.issuetracker.model.CommentModel
 import de.thm.webservices.issuetracker.model.IssueModel
+import de.thm.webservices.issuetracker.model.IssueViewModel
 import de.thm.webservices.issuetracker.service.IssueService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import java.util.*
 
 @RestController
@@ -15,8 +17,14 @@ class IssueViewController(
 ) {
 
     @GetMapping("_view/issueboard")
-    fun getIssueData(@RequestParam issues: List<UUID>): Flux<Map<IssueModel, List<CommentModel>>>{
+    fun getIssueData(@RequestParam issues: List<UUID>): Flux<Optional<IssueViewModel>>{
 
-        return Flux.just()
+        return Flux.fromIterable(issues)
+                .flatMap {
+                    issueService.getIssueWithAllComments(it)
+                            .map { Optional.of(it) }
+                            .switchIfEmpty(Mono.just(Optional.empty()))
+                }
+
     }
 }
