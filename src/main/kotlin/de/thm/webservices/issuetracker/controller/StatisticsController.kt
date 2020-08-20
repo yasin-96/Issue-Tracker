0da203a5c7e503a5fe3de3945c693cbb2d2d2a1a
Yasin Eraslan <yasin.eraslan@mni.thm.de>
2020-08-20 22:00:40 +0200

    hopefully works

--- src/main/kotlin/de/thm/webservices/issuetracker/controller/StatisticsController.kt
+++ src/main/kotlin/de/thm/webservices/issuetracker/controller/StatisticsController.kt
@@ -2,10 +2,12 @@ package de.thm.webservices.issuetracker.controller
 
 import de.thm.webservices.issuetracker.model.IssueModel
 import de.thm.webservices.issuetracker.model.StatsModel
+import de.thm.webservices.issuetracker.model.event.TagStatsModel
 import de.thm.webservices.issuetracker.repository.CommentRepository
 import de.thm.webservices.issuetracker.repository.IssueRepository
 import de.thm.webservices.issuetracker.service.CommentService
 import de.thm.webservices.issuetracker.service.IssueService
+import de.thm.webservices.issuetracker.service.TaggingService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -15,7 +17,7 @@ import reactor.kotlin.core.publisher.toMono
 import java.util.*
 
 @RestController
-class StatisticsController(var commentService: CommentService,var issueService: IssueService) {
+class StatisticsController(var commentService: CommentService,var issueService: IssueService, var taggingService: TaggingService) {
 
     @GetMapping("/_stats")
     fun getStatsFromIds(@RequestParam userIds: List<UUID>): Flux<Optional<StatsModel>> {
@@ -36,5 +38,38 @@ class StatisticsController(var commentService: CommentService,var issueService: 
                             }
                 }
     }
-}
+
+    @GetMapping("/_stat/tags")
+    fun getNumberOfTaggedUsers(@RequestParam issueId:UUID):Mono<TagStatsModel> {
+        return commentService.getAllCommentByIssueId(issueId)
+                .map {
+                    taggingService.tagging(it.content).map {
+                        it.count()
+                    }
+                }
+                .count().map {
+                    TagStatsModel(issueId,it.toInt())
+                }
+    }
+
+
+
+
+
+                /*.map { it.flatMap {
+                    taggingService.tagging(it.content).map {
+                        it.count()
+                    }
+                } .map {
+
+                }
+
+
+
+                }
+
+
+                }*/
+                }
+
 
