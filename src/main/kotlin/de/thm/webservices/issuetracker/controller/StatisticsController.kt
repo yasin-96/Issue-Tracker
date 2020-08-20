441cd9f1030806ce78dd8de42299fb4c9ce723f9
Yasin Eraslan <yasin.eraslan@mni.thm.de>
2020-08-20 20:57:11 +0200

    bla

--- src/main/kotlin/de/thm/webservices/issuetracker/controller/StatisticsController.kt
+++ src/main/kotlin/de/thm/webservices/issuetracker/controller/StatisticsController.kt
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toFlux
 import reactor.kotlin.core.publisher.toMono
 import java.util.*
 
@@ -17,12 +18,18 @@ import java.util.*
 class StatisticsController(var commentService: CommentService,var issueService: IssueService) {
 
     @GetMapping("/_stats")
-    fun getStatsFromIds(@RequestParam userIds:List<UUID>) : Flux<StatsModel> {
-        return Flux.fromIterable(userIds)
-                .map {
-                    it
-                    }
-                .zipWith(issueService.getAllIssuesFromOwnerById())
-                }
+    fun getStatsFromIds(@RequestParam userIds: List<UUID>): Flux<StatsModel> {
+        return Flux.fromIterable(userIds).
+            flatMap {
+                getStats(it)
+            }
     }
 
+
+    fun getStats(uuid: UUID) : Flux<StatsModel>{
+        return Flux.zip(commentService.getAllCommentsByUserId(uuid).collectList(),issueService.getAllIssuesFromOwnerById(uuid).collectList())
+                .map {
+                    StatsModel(uuid,it.t1.size,it.t2.size)
+                }
+    }
+    }
--- src/main/kotlin/de/thm/webservices/issuetracker/model/StatsModel.kt
+++ src/main/kotlin/de/thm/webservices/issuetracker/model/StatsModel.kt
@@ -1,6 +1,9 @@
 package de.thm.webservices.issuetracker.model
 
+import java.util.*
+
 data class StatsModel (
+        var uuid: UUID,
         var numberOfIssues : Int ,
         var numberOfComments : Int
 )
