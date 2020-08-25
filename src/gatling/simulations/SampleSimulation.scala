d820e02135b4e7556978dff02e2644f0c4607445
Alexander Jajzler <jaijszler@gmail.com>
2020-08-25 15:43:30 +0200

    gatling not work

--- src/gatling/simulations/SampleSimulation.scala
+++ src/gatling/simulations/SampleSimulation.scala
@@ -1,8 +1,8 @@
+
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-/*
-class LoginTest extends Simulation {
+class SampleSimulation extends Simulation {
 
   val sessionHeaders = Map("Authorization" -> "Bearer ${authToken}",
     "Content-Type" -> "application/json")
@@ -29,4 +29,33 @@ class LoginTest extends Simulation {
     )
 
   setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
-}*/
+}
+
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+/*
+class SampleSimulation extends Simulation {
+
+  object Search {
+    val search = exec(
+      http("Login")
+        .post("/auth/login")
+        .formParam("username", "alex")
+        .formParam("password", "alex")
+        .check(jsonPath("$..token").exists.saveAs("authToken"))
+    ).pause(1)
+  }
+
+  val httpProtocol = http
+     http.baseUrl("http://localhost:8080/")
+    .acceptHeader("application/json")
+
+  val users = scenario("User Login").exec(Search.search)
+
+  setUp(users.inject(rampUsers(1) during (1 seconds))).protocols(httpProtocol)
+}
+*/
